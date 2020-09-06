12535aa7310b95ec65f895e4e1a9d955458d3bc0
fuerte <fuejoh4730@gmail.com>
2020-09-05 22:17:54 -0400

    fix index

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var path = require('path');
-var express = express();
 
-// express.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.use(express.static('public'));
-
-// express.get('/', function(req, res){
-//     res.render('home.html');
-// });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
