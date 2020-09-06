04ec5ef9b5000b8e3abf36078e5bc040fdaed698
fuerte <fuejoh4730@gmail.com>
2020-09-05 23:12:33 -0400

    path tries

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ io.on('connection', (socket) => {
   });
 });
 
-app.listen(3000);
-
-
-// http.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
