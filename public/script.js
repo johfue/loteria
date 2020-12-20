4faadef5ff2af1f0172aa65c630f3b091dc81cda
fuerte <fuejoh4730@gmail.com>
2020-12-19 19:00:31 -0500

    alleged winner review card logic

--- public/script.js
+++ public/script.js
@@ -116,6 +116,7 @@ function host() {
         for (q=0; q < disableThis.length; q++) {
             disableThis[q].disabled = true;
         }
+        console.log(_("cardReviewList--alleged"));
         _("cardReviewList--alleged").innerHTML = "";
         for (c=0; c < drawnCards.length; c++) {
             var li = document.createElement("li");
