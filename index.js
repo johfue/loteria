5dc783d0662731ac1959688268e7f8011039d4ed
johfue <fuejoh4730@gmail.com>
2022-10-13 22:47:11 -0500

    player join crash 11

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ io.on('connection', (socket) => {
         socket.to(roomNumber).emit('new player', nickname, socket.id, oldID);
     });
     socket.on('update newcomer', (gameInfo, id, deck) => {
-        io.to(id).emit('catch-up', gameInfo, deckList);
+        io.to(id).emit('catch-up', gameInfo, deck);
     });
     socket.on('game state', (bool, roomNumber) => {
         socket.to(roomNumber).emit('game state', bool);
