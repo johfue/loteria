8b0834bb5c73621e0c3fecae06b42fed4c9f1436
johfue <fuejoh4730@gmail.com>
2023-09-03 14:23:36 -0500

    added db authorization

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const mongoose = require("mongoose");
 
 const server = '127.0.0.1:27017';
 const database = 'Loteria';
+const username = "loteriUser";
+const password = "a1r3t0l";
+
+
 class Database {
   constructor() {
     this._connect();
   }
   _connect() {
     mongoose
-      .connect(`mongodb://${server}/${database}`)
+      .connect(`mongodb://${username}:${password}@{server}/${database}`)
       .then(() => {
         console.log('Database connection successful');
       })
@@ -26,8 +30,6 @@ class Database {
 }
 
 module.exports = new Database();
-
-
 
 // const Schema = mongoose.Schema;
 
