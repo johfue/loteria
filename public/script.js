007be6e02da93393291d41d3fc0aa1557ea52c8f
johfue <fuejoh4730@gmail.com>
2024-05-02 19:20:29 -0400

    added chainable animation logic for wincondition

--- public/script.js
+++ public/script.js
@@ -185,6 +185,7 @@ function animate(elem, animation) {
 }
 
 async function init() {
+    await animate(winConditionInfo, "column");
     await animate(winConditionInfo, "row");
     await animate(winConditionInfo, "diagonal");
     await animate(winConditionInfo, "twoByTwo");
