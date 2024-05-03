f4647f0a7c6b7d5825f3757278e67a9ee8b6f790
johfue <fuejoh4730@gmail.com>
2024-05-03 17:29:25 -0400

    added chainable animation logic for wincondition

--- public/script.js
+++ public/script.js
@@ -218,7 +218,7 @@ function animate(elem, animation) {
 function setWinCondition (conditions) {
     // winConditionInfo.setAttribute("class", "winInfo winInfo--host");
     loopSetWinAnimation(conditions);
-    winConditionInfo.style.animationPlayState === 'running');
+    winConditionInfo.style.animationPlayState === 'running';
     winConditionInfo.classList.add("looping");
 
     // winConditionInfo.classList.add("class", condition);
