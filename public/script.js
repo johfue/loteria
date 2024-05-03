2e1f9f1bde51e62de21eb6eb897dde31455c2743
johfue <fuejoh4730@gmail.com>
2024-05-02 21:32:13 -0400

    added chainable animation logic for wincondition

--- public/script.js
+++ public/script.js
@@ -183,9 +183,9 @@ function animate(elem, animation) {
     //     handleAnimationEnd();
     // }, 500);
     
-    elem.addEventListener("animationend", handleAnimationEnd);
+    // elem.addEventListener("animationend", handleAnimationEnd);
 
-    // elem.addEventListener("animationend", handleAnimationEnd, { once: true });
+    elem.addEventListener("animationend", handleAnimationEnd, { once: true });
     elem.classList.add(animation);
   });
 }
