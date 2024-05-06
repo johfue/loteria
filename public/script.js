243bfff516d9f9033f13689006f629841af4dee3
johfue <fuejoh4730@gmail.com>
2024-05-05 22:25:12 -0400

    moved room generation logic server side

--- public/script.js
+++ public/script.js
@@ -1214,7 +1214,7 @@ _("closeSelect").addEventListener('click', function(event) {
 socket.on('room clear', function(r){
     console.log(r)
     window.history.pushState('','', r);
-    load_page("host", r);
+    load_page("host", r, event);
 
 });
 
